4eb90f0ca2b01bf156b0f321dcbb05c24d837a69
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-12-16 22:58:26 +1100

    format

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ fn main() {
     println!("Welcome to rust-int, a toy program for trapezoidal integration");
 
     let (xmin, xmax, npoints) = read_inputs();
-    
+
     let integral = trapz(&three_x_squared, xmin, xmax, npoints);
 
-    println!("integral value is {}",integral);
-
+    println!("integral value is {}", integral);
 }
 
 // toy functions to integrate
@@ -18,32 +17,31 @@ fn two_x(x: f64) -> f64 {
 }
 #[allow(dead_code)]
 fn ident(x: f64) -> f64 {
-    x 
+    x
 }
 
 fn three_x_squared(x: f64) -> f64 {
-    3.0*x*x
+    3.0 * x * x
 }
 
 fn read_inputs() -> (f64, f64, i32) {
     println!("enter xmin value");
-    let mut xmin =  String::new();
+    let mut xmin = String::new();
     io::stdin().read_line(&mut xmin).expect("failed to read");
     let xmin: f64 = xmin.trim().parse().expect("must be numeric");
     println!("enter xmax value");
-    let mut xmax =  String::new();
+    let mut xmax = String::new();
     io::stdin().read_line(&mut xmax).expect("failed to read");
     let xmax: f64 = xmax.trim().parse().expect("must be numeric");
     println!("enter npoints value");
-    let mut npoints =  String::new();
+    let mut npoints = String::new();
     io::stdin().read_line(&mut npoints).expect("failed to read");
     let npoints: i32 = npoints.trim().parse().expect("must be integer");
     (xmin, xmax, npoints)
 }
 
-
-fn gen_range(xmin: f64, xmax: f64, npoints: i32) -> Vec<f64>  {
-    // generates a range of  npoints+1 values between xmin and xmax 
+fn gen_range(xmin: f64, xmax: f64, npoints: i32) -> Vec<f64> {
+    // generates a range of  npoints+1 values between xmin and xmax
     if xmin >= xmax {
         panic!("xmin is >= xmax, panic!")
     }
@@ -51,7 +49,7 @@ fn gen_range(xmin: f64, xmax: f64, npoints: i32) -> Vec<f64>  {
     let ninter: i32 = npoints + 1;
     let mut range = Vec::with_capacity(ninter.try_into().unwrap());
     for i in 0..ninter {
-        let val = xmin + f64::from(i)*elem;
+        let val = xmin + f64::from(i) * elem;
         range.push(val);
     }
     range
@@ -65,9 +63,7 @@ fn eval_on_range(range: &Vec<f64>, func: &dyn Fn(f64) -> f64) -> Vec<f64> {
     result
 }
 
-
-
-fn trapz(func: &dyn Fn(f64) -> f64, xmin: f64, xmax: f64,  npoints: i32) -> f64 {
+fn trapz(func: &dyn Fn(f64) -> f64, xmin: f64, xmax: f64, npoints: i32) -> f64 {
     // trapezoidal integration of function Fn over domain xmin -> xmax
     // with npoints quadrature points
     println!("xmin is {}", xmin);
@@ -80,8 +76,7 @@ fn trapz(func: &dyn Fn(f64) -> f64, xmin: f64, xmax: f64,  npoints: i32) -> f64 
     let eval = eval_on_range(&range, &func);
 
     let eval_right = &eval[1..end];
-    let eval_left = &eval[0..end-1];
-
+    let eval_left = &eval[0..end - 1];
 
     let dx = (xmax - xmin) / f64::from(npoints);
 
@@ -89,8 +84,7 @@ fn trapz(func: &dyn Fn(f64) -> f64, xmin: f64, xmax: f64,  npoints: i32) -> f64 
     let mut sum: f64 = 0.0;
 
     for (l, r) in eval_left.iter().zip(eval_right.iter()) {
-        sum += dx* ((l + r)/2.0);
+        sum += dx * ((l + r) / 2.0);
     }
     sum
 }
-
